67d575cb9cff2b1450414dab3c6fc471fbe1ffd4
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-13 13:16:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,20 +8,51 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+
+   arr.map( function(student){
+    if(student.marks > 50){
+      console.log(student);
+    }
+   });
+ 
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+
+  arr.forEach(function(student){
+    if(student.marks > 50 ){
+      console.log(student);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  
+    let newStudent = {id:4,name:"susan",age:"20",marks:45};
+    arr.push(newStudent);
+    console.log(arr);
+    // console.log(arr);
+  // return arr;
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+   arr = arr.filter(function(student){
+    return student.marks > 50;
+   });
+   
+    console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+    { id: 11, name: "Ram", age: "38", marks: 99 },
+    { id: 12, name: "Sita", age: "30", marks: 95 },
+    { id: 13, name: "Laksman", age: "35", marks: 40 },
+  ]
+  arr = arr.concat(arr2);
+  console.log(arr);
 }
